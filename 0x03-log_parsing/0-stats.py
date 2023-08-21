4a7d0f2a411d61df9674a497e6af12f65510f3a0
Hope Davids <hopeolawole@gmail.com>
2023-08-21 18:32:49 +0000

    modified the stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,44 @@
-#!/usr/bin/python3
-"""
-log parsing
-"""
+#!/usr/bin/env python3
+
+""" This script that reads stdin line by line and computes metrics: """
 
 import sys
-import re
+import signal
+
+total_size = 0
+status_count = {}
 
 
-def output(log: dict) -> None:
+def print_metrics(signum, frame):
+    """ Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+        <status code> <file size> (if the format is not this one, the line
+        must be skipped). After every 10 lines and/or a keyboard interruption
+        (CTRL + C), print these statistics from the beginning:
     """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+
+    print(f"File size: {total_size}")
+    for status_code in sorted(status_count.keys()):
+        print(f"{status_code}: {status_count[status_code]}")
+    sys.exit(0)
 
 
-if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+# Set up the signal handler for keyboard interruption (CTRL + C)
+signal.signal(signal.SIGINT, print_metrics)
 
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+# Read input lines from stdin
+for line in sys.stdin:
+    parts = line.strip().split()
+    if len(parts) >= 7:
+        ip_address, _, _, _, _, status_code, file_size = parts
+        try:
+            file_size = int(file_size)
+            total_size += file_size
+            status_code = int(status_code)
+            if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
+                status_count[status_code] = status_count.get(status_code, 0) + 1
+        except ValueError:
+            continue
 
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
-
-                # File size
-                log["file_size"] += file_size
-
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
-
-                if (line_count % 10 == 0):
-                    output(log)
-    finally:
-        output(log)
+        # Check if it's time to print metrics
+        if len(status_count) == 8 or total_size >= 10:
+            print_metrics(None, None)
