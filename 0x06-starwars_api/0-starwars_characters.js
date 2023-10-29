c7b519e3c319b47e6621fa463d355a290d4d06e3
root <root@dev0ps.kube>
2023-10-29 15:38:29 +0000

    star wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,22 @@
+#!/usr/bin/node
+
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.log('Usage: node 0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
 const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-const swapiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(swapiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Status Code:', response.statusCode);
-  } else {
-    try {
-      const film = JSON.parse(body);
-      const characters = film.characters;
-
-      if (characters.length === 0) {
-        console.log('No characters found for this movie.');
-      } else {
-        characters.forEach((characterUrl) => {
-          request(characterUrl, (charError, charResponse, charBody) => {
-            if (charError) {
-              console.error('Error:', charError);
-            } else if (charResponse.statusCode !== 200) {
-              console.error('Status Code:', charResponse.statusCode);
-            } else {
-              const character = JSON.parse(charBody);
-              console.log(character.name);
-            }
-          });
-        });
-      }
-    } catch (parseError) {
-      console.error('Error parsing response:', parseError);
-    }
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(characterId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
   }
 });
